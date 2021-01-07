1a50d3352fce25f95a2bee5b11064343e5be92d6
Candice Sambury <candiland@candices-imac.home>
2021-01-06 23:48:47 -0500

    Trying to deploy

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = require('./restaurants_api/app');
 app.use(express.static('build'));
+
+app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(`${__dirname}/build/index.html`));
+  });
